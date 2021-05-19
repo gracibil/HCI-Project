dd18ab625c173edd0198b0f4ce27ad9dc95467ad
gracibil <gracjan5892@gmail.com>
2021-05-19 22:06:43 +0200

    Added interactive buttons

--- profile_creation_page.js
+++ profile_creation_page.js
@@ -2,3 +2,9 @@ function btnColor(btn) {
     var property = document.getElementById(btn);
     property.className = 'toggled' == property.className ? '' : 'toggled';
 }
+
+function changeColor(button) {
+    var property = document.getElementById(button);
+    property.style.backgroundColor = "red";
+
+}
